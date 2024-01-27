32f6a8d63d7e2bb1dba6ef5a8176094c79a14ec6
Soona <code@chainsona.dev>
2024-01-27 14:26:56 +0100

    feat: list and filter nodes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,160 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { Input, Select, Table } from "react-daisyui";
+
+// {
+//   "node_id": "BQofKZeR1UsU2tx27oexqzC3R3rmF5eaETRbfbEHLuat",
+//   "is_discord_verified": true,
+//   "is_up": true,
+//   "status": "top_150",
+//   "uptime": "1022428506",
+//   "total_rewards": "229596397182"
+//   },
+
+// Convert seconds to days hours minutes
+function secondsToDhms(seconds: number) {
+  seconds = Number(seconds) / 1000;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  return `${d} days ${h} hours ${m} minutes`;
+}
+
+// format rewards
+function formatRewards(rewards: string) {
+  return `${(Number(rewards) / 10 ** 9).toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })}`;
+}
+
+type Node = {
+  node_id: string;
+  is_discord_verified: boolean;
+  is_up: boolean;
+  status: string;
+  uptime: string;
+  uptimeStr: string;
+  total_rewards: string;
+};
 
 export default function Home() {
+  const [nodes, setNodes] = useState<Node[]>([]);
+  const [filteredNodes, setFilteredNodes] = useState<Node[]>([]);
+  const [search, setSearch] = useState(localStorage.getItem("search") || "");
+  const [sort, setSort] = useState(
+    localStorage.getItem("sort") || "uptime-desc"
+  );
+
+  useEffect(() => {
+    fetch("https://shdw-rewards-oracle.shdwdrive.com/node-leaderboard")
+      .then((res) => res.json())
+      .then((data) =>
+        setNodes(
+          data.nodes.map((node: Node) => {
+            return {
+              ...node,
+              uptimeStr: secondsToDhms(parseInt(node.uptime)),
+            };
+          }) || []
+        )
+      );
+  }, []);
+
+  useEffect(() => {
+    setFilteredNodes(
+      nodes
+        .filter(
+          (node: Node) =>
+            search === "" ||
+            node.node_id.toLowerCase().includes(search.toLowerCase()) ||
+            search.toLowerCase().split(",").includes(node.node_id.toLowerCase())
+        )
+        .sort((a: Node, b: Node) => {
+          switch (sort) {
+            case "rewards-asc":
+              return Number(a.total_rewards) - Number(b.total_rewards);
+            case "rewards-desc":
+              return Number(b.total_rewards) - Number(a.total_rewards);
+            case "uptime-desc":
+              return Number(b.uptime) - Number(a.uptime);
+            case "uptime-asc":
+              return Number(a.uptime) - Number(b.uptime);
+            default:
+              return 0;
+          }
+        })
+    );
+  }, [search, nodes, sort]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col w-full text-left overflow-x-auto">
+      <div className="flex w-full component-preview p-4 items-center justify-center gap-2 font-sans">
+        <div className="w-full flex flex-row gap-4">
+          <Input
+            id="search"
+            className="w-full px-4 py-2 bg-base-100 rounded-md"
+            value={search}
+            onChange={() => {
+              setSearch(
+                (document.getElementById("search") as HTMLInputElement).value
+              );
+              localStorage.setItem(
+                "search",
+                (document.getElementById("search") as HTMLInputElement).value
+              );
+            }}
+          />
+          <Select
+            className="w-48 px-4 py-2 bg-base-100 rounded-md"
+            onChange={(event) => {
+              setSort(event.target.value);
+              localStorage.setItem("sort", event.target.value);
+            }}
+            value={sort}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <Select.Option value="rewards-desc">
+              Rewards (higher to lower)
+            </Select.Option>
+            <Select.Option value="rewards-asc">
+              Rewards (lower to higher)
+            </Select.Option>
+            <Select.Option value="uptime-desc">
+              Uptime (higher to lower)
+            </Select.Option>
+            <Select.Option value="uptime-asc">
+              Uptime (lower to higher)
+            </Select.Option>
+          </Select>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Table>
+        <Table.Head>
+          <span />
+          <span>Node ID</span>
+          <span>Discord Verified</span>
+          <span>Active</span>
+          <span>Uptime</span>
+          <span>Total Rewards</span>
+        </Table.Head>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Table.Body>
+          {filteredNodes.map((node: Node, i: number) => (
+            <Table.Row key={node.node_id}>
+              <span className="block text-right pr-4">{`#${i + 1}`}</span>
+              <span>{node.node_id}</span>
+              <span>{node.is_discord_verified ? "Yes" : "No"}</span>
+              <span>{node.is_up ? "Yes" : "No"}</span>
+              <span>{node.uptimeStr}</span>
+              <span className="text-right">
+                {formatRewards(node.total_rewards)}
+              </span>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </main>
   );
 }
