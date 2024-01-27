1946a1b2c6a84ba2ef659b7de9083d11d69206eb
Soona <code@chainsona.dev>
2024-01-27 13:29:23 +0100

    chore: install daisyUI

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["react-daisyui"],
+  reactStrictMode: true,
+};
 
 export default nextConfig;
