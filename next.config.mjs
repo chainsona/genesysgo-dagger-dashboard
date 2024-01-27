721be8b1048fdd59d059224a3eb39229ea8cbc79
Soona <code@chainsona.dev>
2024-01-27 21:30:25 +0100

    feat: pwa install

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+import withSerwistInit from "@serwist/next";
+
 const nextConfig = {
   transpilePackages: ["react-daisyui"],
   reactStrictMode: true,
 };
+
+const withSerwist = withSerwistInit({
+  ...nextConfig,
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 
 import { ToastContainer, Slide } from "react-toastify";
@@ -8,9 +8,48 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "D.A.G.G.E.R. Dashboard";
+const APP_DEFAULT_TITLE = "D.A.G.G.E.R. Testnet2 Dashboard";
+const APP_TITLE_TEMPLATE = "%s - D.A.G.G.E.R. Dashboard";
+const APP_DESCRIPTION = "Dashboard for the SHDW Incentivized Testnet V2";
+
 export const metadata: Metadata = {
-  title: "D.A.G.G.E.R. Testnet2 Dashboard",
-  description: "Dashboard for the SHDW Incentivized Testnet V2",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
 };
 
 export default function RootLayout({
@@ -19,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" dir="ltr">
       <body className={inter.className}>
         {children}
         <ToastContainer
