f1e4eb926ba0c6dabcd5ad5089966e1d7fe0c337
Soona <code@chainsona.dev>
2024-01-28 14:24:51 +0100

    feat: copy node id

--- src/app/components/TableRow.tsx
+++ src/app/components/TableRow.tsx
@@ -5,6 +5,7 @@ import { useCallback, useEffect, useState } from "react";
 import { Table } from "react-daisyui";
 
 import { formatNumbers, secondsToDhms } from "../utils/string";
+import { toast } from "react-toastify";
 
 function ellipsis(str: string, max: number) {
   if (str.length <= max) {
@@ -168,9 +169,55 @@ export default function TableRow(props: TableRowProps) {
     >
       <span className="block text-center pr-4 text-lg">#{rank}</span>
       <span className="">
-        <span className="hidden sm:flex text-left items-center">{id}</span>
+        <span className="hidden sm:flex text-left items-center">
+          {id}
+          <div
+            className="pl-2 hover: cursor-pointer"
+            onClick={() => {
+              navigator.clipboard.writeText(id);
+              toast("Copied to clipboard!");
+            }}
+          >
+            <svg
+              stroke="currentColor"
+              fill="none"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+              <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+            </svg>
+          </div>
+        </span>
         <span className="sm:hidden flex text-left items-center">
           {ellipsis(id, 16)}{" "}
+          <div
+            className="pl-2 hover: cursor-pointer"
+            onClick={() => {
+              navigator.clipboard.writeText(id);
+              toast("Copied to clipboard!");
+            }}
+          >
+            <svg
+              stroke="currentColor"
+              fill="none"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+              <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+            </svg>
+          </div>
         </span>
       </span>
       <span className="">
