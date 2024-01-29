1bd8a90ffff9bf30bed4c83f238fb250e5b83efb
Soona <code@chainsona.dev>
2024-01-29 17:42:28 +0100

    refactor: improve component NodeStats

--- src/app/components/NodeStats.tsx
+++ src/app/components/NodeStats.tsx
@@ -10,47 +10,47 @@ type NodeStatsProps = {
 export default function NodeStats(props: NodeStatsProps) {
   const { nodes } = props;
 
+  const stats = [
+    {
+      title: "shdwNodes",
+      value: nodes.filter((node: Node) => node.is_up).length,
+    },
+    {
+      title: "Waiting shdwNodes",
+      value: nodes.filter((node: Node) => node.status === "queued").length,
+    },
+    {
+      title: "Offline shdwNodes",
+      value: nodes.filter((node: Node) => !node.is_up).length,
+    },
+    {
+      title: "Earned Rewards",
+      value: nodes.filter((node: Node) => parseInt(node.total_rewards) > 0)
+        .length,
+    },
+    {
+      title: "Distributed Rewards",
+      value: formatNumbers(
+        nodes.reduce(
+          (acc: number, node: Node) =>
+            acc + parseInt(node.total_rewards) / 10 ** 9,
+          0
+        )
+      ),
+    },
+  ];
+
   return (
-    <div className="flex flex-col sm:flex-row gap-8 items-center justisfy-center">
-      <div className="flex flex-col p-4 bg-gray-950 rounded-xl w-full">
-        <div className="text-gray-400">shdwNodes</div>
-        <div className="text-4xl font-bold text-center">
-          {nodes.filter((node: Node) => node.is_up).length}
+    <div className="flex flex-col sm:flex-row gap-8 justisfy-center">
+      {stats.map((stat, i) => (
+        <div
+          className="flex flex-col gap-4 p-4 bg-gray-950 rounded-xl w-full"
+          key={i}
+        >
+          <div className="text-gray-400">{stat.title}</div>
+          <div className="text-4xl font-bold text-center">{stat.value}</div>
         </div>
-      </div>
-      <div className="flex flex-col p-4 bg-gray-950 rounded-xl w-full">
-        <div className="text-gray-400">Waiting shdwNodes</div>
-        <div className="text-4xl font-bold text-center">
-          {nodes.filter((node: Node) => node.status === "queued").length}
-        </div>
-      </div>
-      <div className="flex flex-col p-4 bg-gray-950 rounded-xl w-full">
-        <div className="text-gray-400">Offline shdwNodes</div>
-        <div className="text-4xl font-bold text-center">
-          {nodes.filter((node: Node) => !node.is_up).length}
-        </div>
-      </div>
-      <div className="flex flex-col p-4 bg-gray-950 rounded-xl w-full">
-        <div className="text-gray-400">Earned Rewards</div>
-        <div className="text-4xl font-bold text-center">
-          {
-            nodes.filter((node: Node) => parseInt(node.total_rewards) > 0)
-              .length
-          }
-        </div>
-      </div>
-      <div className="flex flex-col p-4 bg-gray-950 rounded-xl w-full">
-        <div className="text-gray-400">Distributed Rewards</div>
-        <div className="text-4xl font-bold text-center">
-          {formatNumbers(
-            nodes.reduce(
-              (acc: number, node: Node) =>
-                acc + parseInt(node.total_rewards) / 10 ** 9,
-              0
-            )
-          )}
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
