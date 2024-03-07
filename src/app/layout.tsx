412eb3ab099c65762521d98cf80bb34a4d87c4b3
Soona <code@chainsona.dev>
2024-03-08 00:55:02 +0100

    refactor(ui): set Roboto as default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "react-toastify/dist/ReactToastify.css";
 import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 import UiLayout from "./components/UiLayout";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata: Metadata = {
   title: "D.A.G.G.E.R. Testnet2 Dashboard",
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <UiLayout>{children}</UiLayout>
         <Analytics />
       </body>
