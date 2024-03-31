517933c79cc5c95dadb65479ad4d31dcc13adb34
Soona <code@chainsona.dev>
2024-03-31 15:51:37 +0200

    feat: cache node versions

--- src/app/api/nodes/versions/route.ts
+++ src/app/api/nodes/versions/route.ts
@@ -1,6 +1,15 @@
 export const dynamic = "force-dynamic"; // defaults to auto
+import { kv } from "@vercel/kv";
 
 export async function GET() {
+  const cached_versions = await kv.get("dagger_versions");
+
+  if (cached_versions) {
+    return Response.json({ data: cached_versions });
+  }
+
+  console.debug("Cache miss, fetching from source");
+
   const versionRes = await fetch(
     "https://dashboard.shdwdrive.com/api/datasources/proxy/uid/ab139ce3-a132-4981-afad-bcaeb7c5b453"
   );
@@ -11,6 +20,10 @@ export async function GET() {
       count: parseInt(d.count),
       version: d.wield_version,
     };
+  });
+
+  await kv.set("dagger_versions", JSON.stringify(versions), {
+    ex: 60,
   });
 
   return Response.json({ data: versions });
